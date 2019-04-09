5ae14e930b59ac8c5d772dab1d3931952c584375
jandre-d <jandre-d@student.codam.nl>
2019-04-09 20:42:37 +0200

    all  int conversions finished

--- convert/int_base_10_signed.c
+++ convert/int_base_10_signed.c
@@ -6,79 +6,46 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/09 17:53:17 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/04/09 19:49:57 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/04/09 20:40:23 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-// static inline void set_res_str_len(t_conversion_input *inp, int *i, int64_t *value)
-// {
-// 	int64_t my_value;
-
-// 	*i = 0;
-// 	my_value = inp->int_value;
-// 	if (my_value < 0)
-// 	{
-// 		*i = 1;
-// 		my_value = my_value < -9223372036854775807 ?
-// 			9223372036854775807 : -my_value;
-// 	}
-// 	else if (value == 0)
-// 		*i = 1;
-// 	else
-// 		while (*value > 0)
-// 		{
-// 			*value /= 10;
-// 			*i += 1;
-// 		}
-// }
-
-// t_bool	convert_int_base_10_signed(t_conversion_result *res, t_conversion_input *inp)
-// {
-// 	char		*character_set;
-// 	int64_t	value;
-// 	int			i;
-
-// 	set_res_str_len(inp, &i, &value);
-// 	res->len = i;
-// 	res->str = TAKE_MULTI(char, i + 1, "convert_int_base_10_unsigned");
-// 	if (res->str == NULL)
-// 		return (FALSE);
-// 	i--;
-// 	value = inp->int_value;
-// 	while (i >= 0)
-// 	{
-// 		res->str[i] = value % 10 + '0';
-// 		value /= 10;
-// 		i--;
-// 	}
-// 	if ((int64_t)inp->int_value < 0)
-// 		res->str[i] = '-';
-// 	return (TRUE);
-// }
-
-static inline void set_res_str_len(t_conversion_input *inp, int *i, int64_t *value)
+static inline void	set_res_str_len(t_conversion_input *inp,
+	int *i, int64_t *value)
 {
 	*i = 0;
 	*value = inp->int_value;
 	if (*value < 0)
-	{
-		*i++;
-		*value = *value < -9223372036854775807 ?
- 			9223372036854775807 : -*value;
-	}
+		*i += 1;
 	if (value == 0)
 		*i = 1;
 	else
-		while (*value > 0)
+		while (*value != 0)
 		{
 			*value /= 10;
 			*i += 1;
 		}
 }
 
-t_bool	convert_int_base_10_signed(t_conversion_result *res, t_conversion_input *inp)
+static inline void	set_value(t_conversion_result *res, int64_t *value, int *i)
+{
+	if (*value < 0)
+	{
+		if (*value < -9223372036854775807LL)
+		{
+			*value = 922337203685477580LL;
+			res->str[*i] = '8';
+			i--;
+		}
+		else
+			*value = -*value;
+	}
+}
+
+t_bool				convert_int_base_10_signed(t_conversion_result *res,
+	t_conversion_input *inp)
 {
 	char		*character_set;
 	int64_t		value;
@@ -86,21 +53,19 @@ t_bool	convert_int_base_10_signed(t_conversion_result *res, t_conversion_input *
 
 	set_res_str_len(inp, &i, &value);
 	res->len = i;
-	res->str = TAKE_MULTI(char, i + 1, "convert_int_base_10_unsigned");
+	res->str = TAKE_MULTI(char, i + 1, "convert_int_base_10_signed");
 	if (res->str == NULL)
 		return (FALSE);
 	i--;
 	value = inp->int_value;
-	if (value < 0)
-	{
-		value = value < -9223372036854775807 ?
- 			9223372036854775807 : -value;
-	}
+	set_value(res, &value, &i);
 	while (i >= 0)
 	{
 		res->str[i] = value % 10 + '0';
 		value /= 10;
 		i--;
 	}
+	if (inp->int_value < 0)
+		res->str[i] = '-';
 	return (TRUE);
 }
--- printf_dump/testmain.c
+++ printf_dump/testmain.c
@@ -6,46 +6,68 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/09 17:31:35 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/04/09 19:34:49 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/04/09 20:39:56 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../ft_printf.h"
 
-static inline void set_res_str_len(t_conversion_input *inp, int *i, uint64_t *value)
+static inline void	set_res_str_len(t_conversion_input *inp,
+	int *i, int64_t *value)
 {
 	*i = 0;
 	*value = inp->int_value;
+	if (*value < 0)
+		*i += 1;
 	if (value == 0)
 		*i = 1;
 	else
-		while (*value > 0)
+		while (*value != 0)
 		{
 			*value /= 10;
 			*i += 1;
 		}
 }
 
-t_bool	convert_int_base_10_unsigned(t_conversion_result *res, t_conversion_input *inp)
+static inline void	set_value(t_conversion_result *res, int64_t *value, int *i)
+{
+	if (*value < 0)
+	{
+		if (*value < -9223372036854775807LL)
+		{
+			*value = 922337203685477580LL;
+			res->str[*i] = '8';
+			i--;
+		}
+		else
+			*value = -*value;
+	}
+}
+
+t_bool				convert_int_base_10_signed(t_conversion_result *res,
+	t_conversion_input *inp)
 {
 	char		*character_set;
-	uint64_t	value;
+	int64_t		value;
 	int			i;
 
 	set_res_str_len(inp, &i, &value);
 	res->len = i;
-	res->str = TAKE_MULTI(char, i + 1, "convert_int_base_10_unsigned");
+	res->str = TAKE_MULTI(char, i + 1, "convert_int_base_10_signed");
 	if (res->str == NULL)
 		return (FALSE);
 	i--;
 	value = inp->int_value;
+	set_value(res, &value, &i);
 	while (i >= 0)
 	{
 		res->str[i] = value % 10 + '0';
 		value /= 10;
 		i--;
 	}
+	if (inp->int_value < 0)
+		res->str[i] = '-';
 	return (TRUE);
 }
 
@@ -55,7 +77,7 @@ int main()
 	t_conversion_input	input;
 	
 	input.int_value = 4000;
-	convert_int_base_10_unsigned(&res, &input);
+	convert_int_base_10_signed(&res, &input);
 	printf("%s", res.str);
 	// int64_t x = -9223372036854775808LL;
 	// int i = 0;
