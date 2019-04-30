8aaa345e5cebceeb548f011a8593efeca7a1a703
jandre-d <jandre-d@student.codam.nl>
2019-04-30 14:24:49 +0200

    yes

--- src/pf_convertions/pf_b.c
+++ src/pf_convertions/pf_b.c
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/27 20:02:30 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/04/30 12:40:15 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/04/30 13:19:32 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ bool	pf_b(t_conversion_in *c_in, t_conversion_out *c_out, va_list *argl)
 		((value = (unsigned char)va_arg(*argl, unsigned int))
 			|| 1)) ||
 		(c_in->mod_h && ((value = (unsigned short)va_arg(*argl, unsigned int))
-			| 1))
+			|| 1))
 		|| (c_in->mod_l && ((value = va_arg(*argl, unsigned long)) || 1))
 		|| (c_in->mod_ll && ((value = va_arg(*argl, unsigned long long)) || 1))
 		|| ((value = va_arg(*argl, unsigned int)) || 1))
